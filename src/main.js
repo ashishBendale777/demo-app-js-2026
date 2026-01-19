2c0278e3f075ae06d31cad8968d4c961ec67ede5
technoweit77 <technoweit77@gmail.com>
2026-01-19 11:00:51 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+//vatiable declaration
+// let - const - var
+
+//declaration with intialization
+let a = 23 //datatype declaration inst.
+
+let b//only delaration
+b = 43 //initialisation
+
+let c = a + b
+console.log(a)
+console.log(b)
+console.log("Addition:", c)
+console.log("Addition:", a + b)
