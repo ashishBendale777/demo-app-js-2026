664703775fd7275e2332a3569bd12e19e3ddef95
technoweit77 <technoweit77@gmail.com>
2026-01-28 10:28:06 +0530

    done

--- src/array.js
+++ src/array.js
@@ -1,19 +1,32 @@
 //array === 
-let a = 234 
+let a = 234
 let nums;
 //index is use for accessing elements from array
-nums = [12,54,76,34,57,345,568,3245,47,657]
+nums = [12, 54, 76, 34, 57, 345, 568, 3245, 47, 657]
 console.log(nums[5]);
 console.log(nums[8]);
-nums[7]=333
+nums[7] = 333
 
 console.log(nums.length);
-console.log(nums[nums.length -1]);
-nums[nums.length-4] = 2222
-console.log(nums[nums.length -1]);
+console.log(nums[nums.length - 1]);
+nums[nums.length - 4] = 2222
+console.log(nums[nums.length - 1]);
 
 //loop is use for leterating / visiting / showing 
 //each element of an array
+let count2 = 0
+let count5 = 0
+let count7 = 0
+
 for (let index = 0; index < nums.length; index++) {
-    console.log(nums[[index]]);
+    if (nums[index] % 2 == 0) {
+        count2++
+        break
+    }
+    if (nums[index] % 5 == 0) {
+        count5++
+    }
 }
+
+console.log("Count of 2", count2)
+console.log("Count of 5", count5)
---
+++ src/strings.js
@@ -0,0 +1,20 @@
+// Strings Manipulation
+let city = "Jalgaon is nice city"
+// console.log(typeof(typeof(city)))
+// console.log(typeof(typeof(age)))
+console.log(city.length)
+console.log(city[0])
+console.log(city[3])
+
+// for (let index = 0; index < city.length ; index++) {
+//     console.log(city[index])    
+// }
+
+let friendsNams = ["Ashish", "Suraj", "Satish", "Pawan"]
+for (let index = 0; index < friendsNams.length; index++) {
+    let name = friendsNams[index]
+    if (name[name.length - 1] === 'j') {
+        console.log(name)
+        break
+    }
+}
