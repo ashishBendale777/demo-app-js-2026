7a6e629c195e5a6a75b91d63a6b37fe1ebcae9d2
technoweit77 <technoweit77@gmail.com>
2026-02-02 10:07:24 +0530

    done

--- src/stringCaps.js
+++ src/stringCaps.js
@@ -1,7 +1,19 @@
-let sentence = "i am software engineer"
+let sentence = "I love my bharat"
 let words = sentence.split(" ")
-let lastWord = words[words.length - 1]
-let capWord = lastWord.replace(lastWord.charAt(0), lastWord.charAt(0).toUpperCase())
+// let lastWord = words[words.length - 1]
+// let capWord = lastWord.replace(lastWord.charAt(0), lastWord.charAt(0).toUpperCase())
+// console.log(sentence)
+// let nSentence = sentence.replace(lastWord, capWord)
+// console.log(nSentence)
+// console.log(words)
+
 console.log(sentence)
-let nSentence = sentence.replace(lastWord, capWord)
-console.log(nSentence)
+
+for (let index = 0; index < words.length; index++) {
+    words[index] = words[index].replace(words[index].charAt(0),
+        words[index].charAt(0).toUpperCase())
+}
+
+// console.log(words)
+let newSentence = words.join(" ")
+console.log(newSentence)
