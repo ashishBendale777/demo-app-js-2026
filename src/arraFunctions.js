91f2319f064ba25caa1ecbadb4dab8a8dcc789b0
technoweit77 <technoweit77@gmail.com>
2026-02-05 10:07:03 +0530

    done arra

---
+++ src/arraFunctions.js
@@ -0,0 +1,52 @@
+let names = ["Ashish", "Sam"]
+
+let nums = [324, 54, 223, 43]
+
+console.log(names)
+
+//push=
+names.push("Pawan")
+
+names.push("Dhiraj")
+
+console.log(names)
+
+names.shift()
+names.shift()
+console.log(names)
+
+names.unshift("Amol")
+names.unshift("Aatish")
+
+console.log(names)
+
+names.pop()
+console.log(names)
+
+console.log(names.length)
+
+console.log(names.indexOf("Amol"))
+
+console.log(names.includes("Ravi"))
+
+names.push("Ravi")
+names.push("Mohan")
+names.push("Mohini")
+names.push("Sarika")
+names.push("Rohan")
+
+console.log(names)
+console.log(names.slice(4,7))
+
+let strData = names.join(" ")
+console.log(strData)
+console.log(typeof(strData))
+
+
+
+////
+let nData = [234,54,63,23,565,235,6,53,234,6,5323,5345]
+
+let evens = []
+let odds = []
+
