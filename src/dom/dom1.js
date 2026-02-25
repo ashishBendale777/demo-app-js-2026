01b76071808b657a329b22a422d346f13b41b872
technoweit77 <technoweit77@gmail.com>
2026-02-25 10:36:08 +0530

    dom 1

---
+++ src/dom/dom1.js
@@ -0,0 +1,11 @@
+//DOM Manipulation
+//DOM-Document Object Model
+
+let hMessage = document.getElementById("hMessage")
+hMessage.innerText = "Heelooo From Dom"
+
+let a = 32
+let b = 43
+let ans = a + b
+hMessage.innerText = ans
+
